de4ead1665d1ea5151c97184fd3005e470570853
Konstantin Tuezov <Konstantin_Tuezov@rcomext.com>
2019-05-16 17:48:17 +0300

    Added Main Page

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,11 +1,50 @@
-import React,{Component} from 'react';
+import React, {Component} from 'react';
+import {AppBar, Tabs, Tab, Typography, withStyles} from '@material-ui/core'
+import {makeStyles} from '@material-ui/core/styles';
 
-class Main extends Component{
-    render(){
-        return(
-            <h1>THIS IS MAIN PAGE</h1>
+const styles = theme => ({
+    root: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.paper,
+    },
+});
+
+class Main extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {value: 0}
+    }
+
+    tabContainer = (text) => {
+        return (
+            <Typography component="div" style={{padding: 8 * 3}}>
+                {text}
+            </Typography>
+        );
+    }
+
+    handleChange = (event, newValue) => {
+        this.setState({value: newValue});
+    }
+
+    render() {
+        const {classes} = this.props;
+        return (
+            <div className={classes.root}>
+                <AppBar position="static">
+                    <Tabs value={this.state.value} onChange={this.handleChange}>
+                        <Tab label="Item One"/>
+                        <Tab label="Item Two"/>
+                        <Tab label="Item Three"/>
+                    </Tabs>
+                </AppBar>
+                {this.state.value === 0 && this.tabContainer("Item One")}
+                {this.state.value === 1 && this.tabContainer("Item Two")}
+                {this.state.value === 2 && this.tabContainer("Item Three")}
+            </div>
         )
     }
 }
 
-export default Main;
+export default withStyles(styles)(Main);
